dc32071d68001ee0fe59ccb7ef89f811185cd3a2
Juan-Glezz <jgonram1210@iesmartinezm.es>
2024-01-17 12:54:36 +0100

    Falta checkout para basadas en clase

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
 from . import views
-
+from .views import WelcomeView,ProductosView,CompraView,Post_EditView,Post_eliminarView,Post_Nuevo_View,Log_In_View,TopProducto_Views,Log_outView
 urlpatterns = [
-    path('', views.compra, name='welcome'),
-    path('tienda/', views.compra, name='compra'),
-    path('tienda/admin/productos', views.productos, name='productos'),
-    path('tienda/admin/editar/<int:pk>', views.post_edit, name='editar'),
-    path('tienda/admin/eliminar/<int:pk>', views.post_eliminar, name='eliminar'),
-    path('tienda/admin/nuevo/', views.post_nuevo, name='nuevo'),
+    path('', CompraView.as_view(), name='welcome'),
+    path('tienda/', CompraView.as_view(), name='compra'),
+    path('tienda/admin/productos', ProductosView.as_view(), name='productos'),
+    path('tienda/admin/editar/<int:pk>', Post_EditView.as_view(), name='editar'),
+    path('tienda/admin/eliminar/<int:pk>', Post_eliminarView.as_view(), name='eliminar'),
+    path('tienda/admin/nuevo/', Post_Nuevo_View.as_view(), name='nuevo'),
     path('tienda/mostrarBusqueda/', views.post_buscar, name='buscar'),
-    path('tienda/login/', views.log_in, name='login'),
+    path('tienda/login/', Log_In_View.as_view(), name='login'),
     path('tienda/checkout/<int:pk>/', views.checkout, name='checkout'),
-    path('tienda/logout/', views.log_out, name='logout'),
-    path('tienda/informes/top10Compras/', views.topProductos, name='top_productos'),
+    path('tienda/logout/', Log_outView.as_view(), name='logout'),
+    path('tienda/informes/top10Compras/',TopProducto_Views.as_view(), name='top_productos'),
     path('tienda/informes/top10mejores/', views.topClientes, name='top_clientes'),
     path('tienda/informes/historialCompras/', views.historial, name='historial'),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -1,5 +1,5 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Producto, Cliente, Compra
 from .form import PostProducto, LoginForm, CompraForm, RegistroForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -7,14 +7,18 @@ from django.contrib.admin.views.decorators import staff_member_required
 from django.utils import timezone
 from django.db.models import Count, Sum
 from django.db import transaction
-from django.views.generic import TemplateView
+from .models import Producto, Cliente, Compra, Marca
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView
+from django.views import View
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 # una función que devuelve un archivo HTML almacenado en una carpeta específica
 # cuando se llama desde una URL específica.
 class WelcomeView(TemplateView):
     template_name = "tienda/index.html"
-
 
 
 # @login_required: Esto significa que para acceder a la función "productos",
@@ -28,22 +32,28 @@ class WelcomeView(TemplateView):
 # Esta función recibe un objeto request que contiene la información de la solicitud
 # realizada por el cliente, posteriormente se realiza una consulta a la base de datos
 # para obtener todos los objetos de la clase Producto. El método filter() devuelve una lista
-# de objetos filtrados(En este caso es todo) Y una vez tiene todo los datos los devuelve.
+# de objetos filtrados(En este caso es
 
-@login_required(login_url='/tienda/login/')
-@staff_member_required
-def productos(request):
-    Productos = Producto.objects.filter();
-    return render(request, 'tienda/producto.html', {'Productos': Productos})
+
+class ProductosView(ListView):
+    model = Producto
+    template_name = 'tienda/producto.html'
+    context_object_name = 'Productos'
+
+    @method_decorator(login_required(login_url='/tienda/login/'))
+    @method_decorator(staff_member_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 
 # Esta funcion utiliza la función filter() para obtener todos los objetos de la clase Producto
 # y almacenarlos en la variable Productos. Luego, se utiliza la función render() para renderizar
 # la plantilla tienda/compra.html junto con los productos obtenidos. Estos productos se pasan a
 # la plantilla, con el nombre Productos. Esto permite acceder a los productos en la plantilla para mostrarlos.
-def compra(request):
-    Productos = Producto.objects.filter();
-    return render(request, 'tienda/compra.html', {'Productos': Productos})
+class CompraView(ListView):
+    model = Producto
+    template_name = 'tienda/compra.html'
+    context_object_name = 'Productos'
 
 
 # Esta funcion tiene el parametro pk, que indica el identificador del Producto que se desea editar,
@@ -53,47 +63,44 @@ def compra(request):
 # producto que se desea editar. Si el formulario es válido, los cambios se guardan en la base de datos utilizando
 # form.save() y se redirige al usuario a la página de productos, en caso de que el formulario no sea válido, se
 # renderiza la página de edición nuevamente, pero esta vez con los errores del formulario visibles
-# para que el usuario los corrija. Finalmente se renderiza la plantilla tienda/editar.html
-@login_required(login_url='/tienda/login/')
-@staff_member_required
-def post_edit(request, pk):
-    producto = get_object_or_404(Producto, pk=pk)
-    if request.method == "POST":
-        form = PostProducto(request.POST, instance=producto)
-        if form.is_valid():
-            producto = form.save(commit=False)
-            producto.save()
-            return redirect('productos')
-    else:
-        form = PostProducto(instance=producto)
-    return render(request, 'tienda/editar.html', {'form': form, 'pk': pk})
+# para que el usuario los corrija. Finalmente, se renderiza la plantilla tienda/editar.html
+class Post_EditView(UpdateView):
+    model = Producto
+    template_name = 'tienda/editar.html'
+    form_class = PostProducto
+    success_url = reverse_lazy('productos')
+
+    @method_decorator(login_required(login_url='/tienda/login/'))
+    @method_decorator(staff_member_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 
 # Esta función toma los argumentos request y pk. El pk es la clave primaria del producto que
 # se va a eliminar, se busca el producto en la base de datos usando su clave primaria pk y
 # se elimina usando el método delete() y lo elimina de la base de datos. Una vez eliminado,
 # redirecciona al usuario a la página de productos
-@login_required(login_url='/tienda/login/')
-@staff_member_required
-def post_eliminar(request, pk):
-    producto = Producto.objects.filter(pk=pk).delete()
-    return redirect('productos')
+@method_decorator(login_required(login_url='/tienda/login/'), name='dispatch')
+@method_decorator(staff_member_required, name='dispatch')
+class Post_eliminarView(DeleteView):
+    model = Producto
+    def get(self, request, pk):
+        producto = Producto.objects.filter(pk=pk).delete()
+        return redirect('productos')
 
 
 # Esta funcion hace que cuando se realiza una solicitud POST con datos válidos en el formulario
 # PostProducto, se guarda en la base de datos y se redirige al usuario a la página de productos.
-@login_required(login_url='/tienda/login/')
-@staff_member_required
-def post_nuevo(request):
-    if request.method == 'POST':
-        form = PostProducto(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('productos')
-    else:
-        form = PostProducto()
+class Post_Nuevo_View(CreateView):
+    model = Producto
+    template_name = 'tienda/nuevo.html'
+    form_class = PostProducto
+    success_url = reverse_lazy('productos')
 
-    return render(request, 'tienda/nuevo.html', {'form': form})
+    @method_decorator(login_required(login_url='/tienda/login/'))
+    @method_decorator(staff_member_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
 
 
 # Obtiene el valor del parámetro "buscar_post" de la solicitud utilizando el método GET, con ello
@@ -114,39 +121,28 @@ def post_buscar(request):
 # redirección, autentica al usuario verificando su nombre de usuario y contraseña, inicia sesión si
 # el usuario es válido, muestra un mensaje de error si el usuario no es válido, crea un formulario
 # vacío si la solicitud no es de tipo POST y finalmente renderiza la página de inicio de sesión con el formulario correspondiente.
-def log_in(request):
-    if request.user.is_authenticated:
-        return redirect('compra')
+class Log_In_View(LoginView):
+    template_name = 'tienda/login.html'
 
-    if request.method == 'POST':
-        form = LoginForm(data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            rNext = request.GET.get('next')
-            if rNext is None:
-                rNext = '/tienda'
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect(rNext)
-            else:
-                form.add_error(None, "Nombre de usuario o contraseña no válida")
-                return render(request, "tienda/login.html", {"form": form})
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        nextURL = self.get_success_url()
+        if not nextURL:
+            nextURL = 'tienda/'
+        return redirect(nextURL)
 
-    else:
-        form = LoginForm()
-
-    return render(request, "tienda/login.html", {"form": form})
-
+    def form_invalid(self, form):
+        response = super().form_invalid(form)
+        return response
 
 # Este código es una función llamada log_out que se encarga de cerrar la sesión de un usuario.
 # Primero, se llama a la función logout pasándole el objeto request, lo cual desloguea al
 # usuario actual. Luego, se redirige al usuario a la página de bienvenida utilizando la función redirect
 # pasándole como argumento el nombre de la ruta welcome.
-def log_out(request):
-    logout(request)
-    return redirect('welcome')
+class Log_outView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('welcome')
 
 
 # El código es una función de checkout que recibe una solicitud (request) y un pk como parámetros,
@@ -156,7 +152,7 @@ def log_out(request):
 # se actualizan las cantidades del producto y el saldo del cliente. Finalmente, se redirige a la página
 # de bienvenida. Si la solicitud no es de tipo POST, se renderiza un formulario de compra.
 @transaction.atomic
-@login_required(login_url='/tienda/login/')
+@login_required(login_url='/tienda/tienda/login/')
 def checkout(request, pk):
     producto = get_object_or_404(Producto, pk=pk)
     cliente = get_object_or_404(Cliente, user=request.user)
@@ -186,12 +182,25 @@ def checkout(request, pk):
 # de compras que han tenido en orden descendente y se seleccionan los primeros 10 productos de la
 # lista. Una vez terminado eso, devuelve una respuesta renderizando el template tienda/top10Compras.html
 # y pasando la variable productos para poder acceder a ellos en la plantilla.
-@login_required(login_url='/tienda/login/')
-@staff_member_required
-def topProductos(request):
-    topP = Producto.objects.annotate(sum_unidades=Sum('compra__unidades'),
-                                     sum_importes=Sum('compra__importe')).order_by('-sum_unidades')[:10]
-    return render(request, 'tienda/informe.html', {'topP': topP})
+class TopProducto_Views(View):
+    template_name = 'tienda/informe.html'
+
+    @method_decorator(login_required(login_url='/tienda/registro/login/'))
+    @method_decorator(staff_member_required)
+    def dispatch(self, *args, **kwargs):
+        return super().dispatch(*args, **kwargs)
+
+    def get(self, request):
+        topP = Producto.objects.annotate(purchase_count=Count('compra')).order_by('-purchase_count')[10]
+        return render(request, self.template_name, {'topP': topP})
+
+
+# @login_required(login_url='/tienda/login/')
+# @staff_member_required
+# def topProductos(request):
+#     topP = Producto.objects.annotate(sum_unidades=Sum('compra__unidades'),
+#                                      sum_importes=Sum('compra__importe')).order_by('-sum_unidades')[:10]
+#     return render(request, 'tienda/informe.html', {'topP': topP})
 
 
 # La función, realiza una consulta a la base de datos para obtener los 10 mejores clientes.
@@ -215,6 +224,3 @@ def topClientes(request):
 def historial(request):
     compras = Compra.objects.all().order_by('-fecha')
     return render(request, 'tienda/informe.html', {'compras': compras})
-
-
-
